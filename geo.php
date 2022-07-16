2d3a66e482ec1a022ce4f4dcb9e76506fbf435f5
Muhindi Njenga <91414809+Jackasoft@users.noreply.github.com>
2022-07-16 14:09:09 +0300

    Add files via upload

---
+++ geo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jackasoft Geolocation API!</title>
+</head>
+<body onload="getLocation()">
+    <h4><em>Getting geolocation of the users</em></h4>
+    <p>longitude & latitude</p>
+
+<p id="demo"></p>
+
+<script src="geo.js"></script>
+
+</body>
+</html>
